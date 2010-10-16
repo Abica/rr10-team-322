1598ca5dc0ff65972d411c24f28d16457fa32f75
Nicholas Wright <nick.loves.rails@gmail.com>
2010-10-16 02:01:33 -0600

    add bundler gemfile and app.rb shell

---
+++ app.rb
@@ -0,0 +1,34 @@
+require 'sinatra'
+require 'haml'
+require 'sass'
+require 'json'
+require 'sqlite3'
+require 'datamapper'
+require 'pp'
+
+configure do
+  set :sessions , true
+
+  DataMapper::Logger.new($stdout, :debug)
+  DataMapper.setup( :default , "sqlite3://#{Dir.pwd}/development.sqlite3" )
+  DataMapper.finalize
+  DataMapper.auto_upgrade!
+end
+
+error do
+  e = request.env['sinatra.error']
+  Kernel.puts e.backtrace.join("\n")
+  'Application error'
+end
+
+helpers do
+end
+
+get '/' do
+  haml :index, :layout => true
+end
+
+get '/main.css' do
+  content_type 'text/css', :charset => 'utf-8'
+  sass :stylesheet
+end
